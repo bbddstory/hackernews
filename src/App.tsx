c0e138fcd8f51b8687e96532082cdd9e793a8764
bbddstory <bbddstory@gmail.com>
2020-05-25 09:12:15 +1000

    feat(App): setup App style and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** 
+  * @desc Application root
+  * @author Leon Li bbddstory@live.com
+  */
+import React, { useEffect } from 'react';
+import { Router } from '@reach/router';
+import { requestInterceptor, responseInterceptor } from './api/interceptor';
+import NewsList from './components/NewsList/NewsList';
+import CommentsList from './components/CommentsList/CommentsList';
+import './App.scss';
 
-function App() {
+export const App: React.FC = () => {
+  // Axios global interceptor setup
+  useEffect(() => {
+    requestInterceptor(); // Request interceptor
+    responseInterceptor(); // Response interceptor
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NewsList path="/" />
+      <CommentsList path="comments/:itemId" />
+    </Router>
   );
 }
 
